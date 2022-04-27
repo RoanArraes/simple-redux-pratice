67382d1778dd0b8f3729939e83bd9c9f67077232
RoanArraes <roan.arraes@hotmail.com>
2022-04-27 15:04:49 -0300

    adjust colors and constants

---
+++ src/common/theme.ts
@@ -0,0 +1,26 @@
+const theme = {
+  colors: {
+    background: '#433A3F',
+    background2: '#3D5A6C',
+    white: '#FFFF',
+    black: '#000'
+  },
+  counter: {
+    labelValueColor: '#CBEF43',
+    buttonCounterSetValue: {
+      backgroundColor: '#8DE969',
+      borderColor: '#000',
+      labelColor: '#FFFF',
+      labelColorHover: '#000'
+    }
+  },
+  fontSizes: {
+    small: '0.8em',
+    medium: '1.2em',
+    larger: '1.8em',
+    big: '2.3em',
+    extreme: '8em'
+  }
+};
+
+export default theme;
